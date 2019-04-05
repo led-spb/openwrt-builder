9b877862c9c239d1962591608befd1c6c87faafa
Alexey.Ponimash <Alexey.Ponimash@gmail.com>
2019-04-05 16:55:54 +0300

    Wifi password choose

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
-
-BIN_DIR="$(dirname $0)/images"
+cd $(dirname $0)
 . ./config
+
+if [ ! -f .password ]; then
+  echo Create .password in current directory with WIFI password
+  exit 1
+fi
+
+
+WIFI_PASSW=$(cat .password)
+rm -rf ./bin/files 2>/dev/null
+cp -r ./files bin/
+sed -i "s/#WIFI_PASSW#/$WIFI_PASSW/" ./bin/files/etc/config/wireless
+
 
 cd ./bin
 make clean
-make image PROFILE="$PROFILE" EXTRA_IMAGE_NAME="$IMAGE_NAME" PACKAGES="$PACKAGES"
+make image PROFILE="$PROFILE" EXTRA_IMAGE_NAME="$IMAGE_NAME" PACKAGES="$PACKAGES" FILES="files/"
